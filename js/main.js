272627b343223937a7128e962295422f51fb8afb
Haley Billson <h_billson@fanshaweonline.ca>
2020-10-13 18:05:51 -0400

    adding dynamic data

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 // import your packages here
-import Person from "./modules/DataModule.js";
-import NavSystem from "./modules/TheNavSystem.js";
+import Team from "./modules/DataModule.js";
+// import NavSystem from "./modules/TheNavSystem.js";
 
 (() => {
     // stub * just a place for non-component-specific stuff
-    console.log('loaded');
+    
+    let userSection = document.querySelector(".users-section"),
+        userTemplate = document.querySelector("#profs-template").content;
 
-    console.log(Person);
+    debugger;
+
+    // select our user elements and load the content
+
+    function handleDataSet(data) {
+        for (let user in Team) {
+            let currentUser = userTemplate.cloneNode(true),
+            currentUserText = currentUser.querySelector('.user').children;
+            
+            currentUserText[1].textContent = Team[user].name;
+            currentUserText[2].textContent = Team[user].role;
+            currentUserText[3].textContent = Team[user].nickname;
+
+            userSection.appendChild(currentUser);
+        }
+
+
+     
+    }
+
+    handleDataSet();
+
 })();
--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,31 @@
 export default {
-    name: "Haley",
-    role: "Student",
-    nickname: "Haley"
+    Haley: {
+        name: "Haley",
+        role: "Main Character",
+        nickname: "Moose"
+    },
+
+    Amanda: {
+        name: "Amanda",
+        role: "Bro",
+        nickname: "Manda <3333"
+    },
+
+    Jae: {
+        name: "Jae-lyn", 
+        role: "Bestie",
+        nickname: "Jae"
+    },
+
+    Kenobi: {
+        name: "Kenobi",
+        role: "Cat",
+        nickname: "Son"
+    },
+
+    Kai: {
+        name: "Kai", 
+        role: "Kitten",
+        nickname: "Rascal"
+    },
 }
